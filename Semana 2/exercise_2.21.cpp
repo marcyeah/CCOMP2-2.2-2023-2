12232db879bc6d484c9efd5c24dc188b86ee9f36
marcyeah <128000400+marcyeah@users.noreply.github.com>
2023-09-04 21:47:34 -0500

    Delete Semana 2 directory

--- Semana 2/exercise_2.21.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "  CCC   +       +\n C      +       +\nC     +++++   +++++\n C      +       +\n  CCC   +       +" << endl;
-    
-    return 0;
-}
